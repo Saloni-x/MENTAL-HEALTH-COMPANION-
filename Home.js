100ca7e01e50423b648d0c9eda52780e27927430
Saloni <s69056298@gmail.com>
2025-11-07 23:15:35 +0530

    Update Home.js

--- Home.js
+++ Home.js
@@ -3,10 +3,11 @@ import React from "react";
 function Home() {
   return (
     <div style={{ padding: "20px" }}>
-      <h1>Welcome to MoodMate 💜</h1>
+      <h1>Welcome to MoodMate </h1>
       <p>This app helps you track your mood and chat with a supportive bot.</p>
     </div>
   );
 }
+
 
 export default Home;
