42a99175bd508bd5800ffc3f4c2137ee563fa44d
Saloni <s69056298@gmail.com>
2025-11-07 23:15:12 +0530

    Update MoodGraph.js

--- MoodGraph.js
+++ MoodGraph.js
@@ -4,19 +4,20 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsi
 const MoodGraph = ({ moodData }) => {
   return (
     <div className="mood-graph">
-      <h2>Your Mood Tracker 📊</h2>
+      <h2>Your Mood Tracker </h2>
       <ResponsiveContainer width="100%" height={300}>
         <LineChart data={moodData}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="time" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="mood" stroke="#8884d8" activeDot={{ r: 8 }} />
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="time" />
+       <YAxis />
+    <Tooltip />
+    <Legend />
+    <Line type="monotone" dataKey="mood" stroke="#8884d8" activeDot={{ r: 8 }} />
         </LineChart>
       </ResponsiveContainer>
     </div>
   );
 };
+
 
 export default MoodGraph;
