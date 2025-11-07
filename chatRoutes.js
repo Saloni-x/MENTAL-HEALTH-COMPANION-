32f605c83b987d9b881674a2a707993e57438e97
Saloni <s69056298@gmail.com>
2025-11-07 23:31:53 +0530

    Update chatRoutes.js

--- chatRoutes.js
+++ chatRoutes.js
@@ -2,23 +2,18 @@ import express from "express";
 import Chat from "../models/Chat.js";
 
 const router = express.Router();
-
-// Dummy AI logic (replace later with real AI)
 function generateBotReply(message) {
   if (message.toLowerCase().includes("stress")) {
-    return "I sense you are stressed. Take a deep breath 🌿";
+    return "I sense you are stressed. Take a deep breath ";
   }
-  return "Thanks for sharing. I’m here for you 💙";
+  return "Thanks for sharing. I’m here for you";
 }
-
 router.post("/", async (req, res) => {
   const { userMessage } = req.body;
   const botReply = generateBotReply(userMessage);
 
   const newChat = new Chat({ userMessage, botReply });
   await newChat.save();
-
-  res.json(newChat);
+res.json(newChat);
 });
-
 export default router;
